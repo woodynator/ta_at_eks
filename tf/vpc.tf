d41ade91620d714a1da7ac4a27b3e6e495f3f3d2
Jonatan Madera <jonamade2@gmail.com>
2022-03-04 17:18:08 +0100

    some changes

--- tf/vpc.tf
+++ tf/vpc.tf
@@ -22,11 +22,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name                 = "ta-vpc"
-  cidr                 = "10.0.0.0/16"
+  name                 = "ta-vpc-kube"
+  cidr                 = "10.1.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets      = ["10.1.1.0/24", "10.1.2.0/24"]
+  public_subnets       = ["10.1.4.0/24", "10.1.5.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
